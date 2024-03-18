0716d7c7fafa16d439e9d34f67f6796dc5fa1a8e
richardarpino <56154705+richardarpino@users.noreply.github.com>
2024-03-18 19:26:22 +0000

    Added comments and basic error checking
    Added some comments to describe functions and added some range fixing to prevent errors if a value is provided that is out of range

--- mlp201127.py
+++ mlp201127.py
@@ -9,10 +9,15 @@ class PowerDisplay(max7219.SevenSegment):
     Bar_3 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 128, 192, 224, 240, 248, 252, 254, 255]
 
     SCAN_LIMIT = 7
-    
-    def __init__(self, cs, spi_bus, speaker=None):
+
+    # Init Class with pins that MLP201127 is connected to
+    def __init__(self, cs: Pin, spi_bus: Pin, speaker: Pin=None):
         
-        # NOTE: We have to re-implement basically what is in our base class init as I found
+        # cs: Clock Pin e.g. cs = Pin(5, Pin.OUT)
+        # spi_bus: SPI Bus e.g SPI(0, baudrate=10000000, polarity=1, phase=1, sck=Pin(2), mosi=Pin(3))
+        # speaker: Speaker Pin e.g Pin(7, Pin.OUT)
+        
+        # NOTE: We have to re-implement what is in our base class init as I found
         # this board gets a bit weird on power cycle unless you send the mosi and sck pins
         # rather than use the defaults - which is what max7219.SevenSegment does
         self.scan_digits = self.digits = 4
@@ -31,22 +36,29 @@ class PowerDisplay(max7219.SevenSegment):
         self.brightness(7)                        # intensity: range: 0..15
         self.reset()
 
+    # reset - reset all the registers on the board or just the leds (you can use the base classes' 'clear' for just the digits)
     def reset(self, led_only:bool = False):
-        for register in range(5, 9):
+        for register in range(5, 8):
              self._write([register, 0x0])
         if not led_only:
             super(PowerDisplay, self).clear(True)
 
+    # set_power - sets the number of leds to illuminate (0-24). Any values over 24 will be treated as 24 and no error will be thrown
     def set_power(self, value):
-        self._write(bytearray([5, PowerDisplay.Bar_1[value]]))
-        self._write(bytearray([6, PowerDisplay.Bar_2[value]]))
-        self._write(bytearray([7, PowerDisplay.Bar_3[value]]))
+        setting = value
+        if setting >= len(PowerDisplay.Bar_1):
+            setting = len(PowerDisplay.Bar_1) - 1
+        self._write(bytearray([5, PowerDisplay.Bar_1[setting]]))
+        self._write(bytearray([6, PowerDisplay.Bar_2[setting]]))
+        self._write(bytearray([7, PowerDisplay.Bar_3[setting]]))
 
+    # tone - emit a 300ms tone from the onboard speaker (if Pin is provided)
     def tone(self):
         if self._speaker:
             self._speaker.on()
             time.sleep_ms(350)
             self._speaker.off()
-
+    
+    # test - illuminate everything as a display test
     def test(self):
         self._write([max7219.MAX7219_REG_DISPLAYTEST, 0x1])
